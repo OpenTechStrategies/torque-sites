c8fb13d73dfda54bb2b40adef18c54b7f011b18e
James Vasile <james@jamesvasile.com>
2017-04-27 11:05:48 -0400

    Add test for table problem
    Test for unclosed table tags.
    
    There are none, which makes sense since the html is well-formed.
    There are still tbody tags littered in the text, but that's a
    different problem.  Maybe it's time to remove --show-table-problem or
    maybe to move it into the test.

--- test_fix-csv.py
+++ test_fix-csv.py
@@ -6,8 +6,20 @@ import imp
 # this is basically just a regular old import:
 fix_csv = imp.load_source('fix_csv', 'fix-csv')
 
+fixed = ""
+def get_fixed():
+    """Do the fix_csv once and then we can run tests on the results.  Note the pare=100.  There should be a test option for quick vs thorough that adjusts pare."""
+    global fixed
+    if not fixed:
+        fixed = fix_csv.fix_csv("100andchangeExport-all-judges.csv", None, 100).getvalue()
+    return fixed
+
 def test_fix_quotes():
-    l = fix_csv.fix_csv("100andchangeExport-all-judges.csv", None, 100)
+    fixed = get_fixed()
     print r'''There shouldn't be any "" or \"" after we fix the csv.''' 
-    assert not "" in l.getvalue()
+    assert not "" in fixed
     
+def test_table_problem():
+    fixed = get_fixed()
+    print "Every table tag should be closed throughout the input."
+    assert fixed.count("<table") == fixed.count("</table>")
