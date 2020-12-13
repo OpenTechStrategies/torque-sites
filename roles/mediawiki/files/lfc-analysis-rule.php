01f7972470f0af66c4381f0e27c6d99389950b1f
Frank Duncan <frank@kank.net>
2020-12-13 11:19:30 -0600

    Update LFC Analysis to be more dynamic
    Because individual competitions may want to add their own tabs, the lfc
    anaylysis needs to be dynamic on an array, rather than explicitly adding
    tabs.  This was mainly needed for 100Change2020, but will probably be
    necessary for future competitions as individual pdfs get their own tabs

--- roles/mediawiki/files/lfc-analysis-rule.php
+++ roles/mediawiki/files/lfc-analysis-rule.php
@@ -1,58 +1,89 @@
 <?php
 # This establishes some new tabs, and removes the "Talk" tab for mediawiki pages.
 #
-# The two tabs are the LFC Analysis and Extensions pages.  This code looks for those pages
-# to exist on the wiki, and if they do, adds them to the tabs at the top.  These pages
-# are for supplemental information to the proposals, but which didn't belong on the proposal
-# pages, and yet should be visually adjacent.
+# The two default tabs are the LFC Analysis and Extensions pages.
+#
+# This code works on the $wgLFCExtraTabs variable.  It should be a list of three item lists:
+# * the first is the prepend of the proposal name
+# * the second item will be the name of a tab linking to that page, if a page is found
+# * the third is the namespace (conventionally one word, all lowercase, unique) for styling
+#
+# These pages are for supplemental information to the proposals, but which didn't belong on
+# the proposal pages, and yet should be visually adjacent.
+#
+# You can extend this variable by doing something the lines of:
+#
+# $wgLFCExtraTabs[] = ["Page name prepend ", "Tab Name", "tabnamespace"];
+#
+# If one of the tabs included has the namespace 'main', then it will override
+# the configuration for the default tab, such as showing up in a different location,
+# with a different name.
+#
+# There's a codependency with the ETL pipelines that these pages actually
+# need to exist on the wiki.
+
+# The tabs get ordered in the order of the array.  This means you need to overwrite it
+# completely if you'd like these tabs to appear elsewhere in the order
+$wgLFCExtraTabs = [
+  ["LFC Analysis of ", "LFC Analysis", "lfcanalysis"],
+  ["Evaluations of ", "Evaluations", "evaluations"]
+];
 
 $wgHooks['SkinTemplateNavigation'][] = function ( $template, &$links ) {
+  # Proposals should only be living in the main namespace
+  if(!array_key_exists('main', $links['namespaces'])) {
+    return;
+  }
+  global $wgLFCExtraTabs;
+
   $currentTitle = $template->getTitle()->getFullText();
 
-  # There's a codependency with the ETL pipelines that these pages actually
-  # need to exist on the wiki.
-  $lfc_prepend = "LFC Analysis of ";
-  $eval_prepend = "Evaluations of ";
-
-  $isAnalysisPage = (strlen($currentTitle) > strlen($lfc_prepend) && substr($currentTitle, 0, strlen($lfc_prepend)) === $lfc_prepend);
-  $isEvalPage = (strlen($currentTitle) > strlen($eval_prepend) && substr($currentTitle, 0, strlen($eval_prepend)) === $eval_prepend);
-
   $originalPageTitle = $currentTitle;
+  $main_tab_name = 'Proposal';
 
-  # If we're on the analysis page, or the evaluation page, then the main page
-  # should be the original proposal.  If we didn't have this, the main page would be
-  # the page we're looking at, even with we're on the Analysis Page/Evaluations Page.
-  if($isAnalysisPage) {
-    $originalPageTitle = substr($currentTitle, strlen($lfc_prepend));
-    $links['namespaces']['main']['class'] = '';
-    $links['namespaces']['main']['href'] = Title::newFromText($originalPageTitle)->getLocalUrl();
-  } else if($isEvalPage) {
-    $originalPageTitle = substr($currentTitle, strlen($eval_prepend));
-    $links['namespaces']['main']['class'] = '';
-    $links['namespaces']['main']['href'] = Title::newFromText($originalPageTitle)->getLocalUrl();
-  }
-  $lfcAnalysisTitle = Title::newFromText($lfc_prepend . $originalPageTitle);
-  $evalTitle = Title::newFromText($eval_prepend . $originalPageTitle);
+  foreach($wgLFCExtraTabs as $tab) {
+    if($tab[2] == 'main') {
+      $main_tab_name = $tab[1];
+    }
+    $prepend = $tab[0];
 
-  # If we're on the Analysis page, then we shouldn't add another analysis page for the analysis
-  # page were on.  Visually, we should be on the tab, with the left tab being the proposal
-  if($isAnalysisPage || $lfcAnalysisTitle->exists()) {
-    $links['namespaces']['main']['text'] = 'Proposal';
-    $links['namespaces']['lfcanalysis'] = [
-     'class' => ($isAnalysisPage ? 'selected' : ''),
-     'href' => ($isAnalysisPage ? $template->getTitle()->getLocalUrl() : $lfcAnalysisTitle->getLocalUrl()),
-     'text' => 'LFC Analysis',
-    ];
+    # If we're on a tab, then the main page # should be the original proposal.  If we
+    # didn't have this, the main page would be the page we're looking at, even when
+    # we're on a tab page (amusingly making new tabs for "Evaluations of Evaluations of <proposal")
+    if (strlen($currentTitle) > strlen($prepend) && substr($currentTitle, 0, strlen($prepend)) === $prepend) {
+      $originalPageTitle = substr($currentTitle, strlen($prepend));
+      $links['namespaces']['main']['class'] = '';
+      $links['namespaces']['main']['href'] = Title::newFromText($originalPageTitle)->getLocalUrl();
+      break;
+    }
   }
 
-  # Same as above, but with Evaluations
-  if($isEvalPage || $evalTitle->exists()) {
-    $links['namespaces']['main']['text'] = 'Proposal';
-    $links['namespaces']['evaluations'] = [
-     'class' => ($isEvalPage ? 'selected' : ''),
-     'href' => ($isEvalPage ? $template->getTitle()->getLocalUrl() : $evalTitle->getLocalUrl()),
-     'text' => 'Evaluations',
-    ];
+  # We need two loops because the first loop ascertains the original proposal title
+  foreach($wgLFCExtraTabs as $tab) {
+    if($tab[2] == 'main') {
+      $main_tab = $links['namespaces']['main'];
+      unset($links['namespaces']['main']);
+      $links['namespaces']['main'] = $main_tab;
+      continue;
+    }
+
+    $prepend = $tab[0];
+    $tab_name = $tab[1];
+    $tab_namespace = $tab[2];
+
+    $tab_title = Title::newFromText($prepend . $originalPageTitle);
+    $on_this_tab = ($currentTitle == $prepend . $originalPageTitle);
+
+    # Add the tab to the top of the page.  If happen to be on the tab, then we should
+    # reflect that visually.
+    if($on_this_tab || $tab_title->exists()) {
+      $links['namespaces']['main']['text'] = $main_tab_name;
+      $links['namespaces'][$tab_namespace] = [
+       'class' => ($on_this_tab ? 'selected' : ''),
+       'href' => ($on_this_tab ? $template->getTitle()->getLocalUrl() : $tab_title->getLocalUrl()),
+       'text' => $tab_name,
+      ];
+    }
   }
 
   # Remove the talk tab, even if the talk page exists
