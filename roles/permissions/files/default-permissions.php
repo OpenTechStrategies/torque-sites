e6dfa649682ca9c5653955c259a079957edd71f3
Frank Duncan <frank@kank.net>
2021-10-21 08:13:08 -0500

    Preparing for the LFC group rebuild
    This change keeps both the old groups, and adds the new ones, so that
    LFC can change their okta set up and nothing is lost.
    
    In addition, groups that aren't specified now default to "LfcOkta" to
    make the user groups more consuable when looking at the user list.
    
    The change from DecisionMakers.css to Restricted.css is because now it's
    for most everyone who has a restricted UI (non admins).

--- roles/permissions/files/default-permissions.php
+++ roles/permissions/files/default-permissions.php
@@ -1,12 +1,23 @@
 <?php
 # Even if these groups aren't used in the wiki (no users assigned), this should be the
 # master list of all groups enabled on the wiki
+
+# Deprecated groups
 $wgGroupPermissions['LFCConsultingPartners']['read'] = true;
 $wgGroupPermissions['LFCResearchPartners']['read'] = true;
 $wgGroupPermissions['LFCEvaluators']['read'] = true;
 $wgGroupPermissions['PseudoDecisionMakers']['read'] = true;
 $wgGroupPermissions['DecisionMakers']['read'] = true;
 $wgGroupPermissions['OutsideReviewers']['read'] = true;
+
+# Groups after reorg
+$wgGroupPermissions["TorqueAdmin"]['read'] = true;
+$wgGroupPermissions["LfcStaff"]['read'] = true;
+$wgGroupPermissions["TorqueDecisionMaker"]['read'] = true;
+$wgGroupPermissions["TorquePartner"]['read'] = true;
+$wgGroupPermissions["TorqueDiligenceFinancial"]['read'] = true;
+$wgGroupPermissions["TorqueDiligenceNonFinancial"]['read'] = true;
+$wgGroupPermissions["TorqueBasic"]['read'] = true;
 
 $wgGroupPermissions['*']['read'] = false;
 $wgGroupPermissions['bot']['protect'] = true;
@@ -30,7 +41,9 @@ $wgGroupPermissions['API_OTS']['read'] = true;
 $wgAvailableRights[] = 'view-special-log';
 $wgGroupPermissions['*']['view-special-log'] = false;
 
+
 # Configuration of different user groups
+# DEPRECATED
 $wgGroupPermissions['sysop']['generated'] = true;
 $wgGroupPermissions['sysop']['edittorqueconfig'] = true;
 $wgGroupPermissions['sysop']['torquedataconnect-admin'] = true;
@@ -41,20 +54,44 @@ $wgGroupPermissions['sysop']['picksome-admin'] = true;
 $wgGroupPermissions['sysop']['view-special-log'] = true;
 $wgGroupPermissions['sysop']['torquedataconnect-edit'] = true;
 $wgGroupPermissions['sysop']['emulategroup'] = true;
-
 $wgGroupPermissions['DecisionMakers']['teamcomment'] = true;
 $wgGroupPermissions['DecisionMakers']['teamcommentseeusernames'] = true;
 $wgGroupPermissions['DecisionMakers']['picksome'] = true;
 $wgGroupPermissions['DecisionMakers']['picksome-write'] = true;
-
 $wgGroupPermissions['PseudoDecisionMakers']['teamcomment'] = true;
 $wgGroupPermissions['PseudoDecisionMakers']['teamcommentseeusernames'] = true;
-
 $wgGroupPermissions['LFCConsultingPartners']['torquedataconnect-edit'] = true;
-
 $wgGroupPermissions['LFCResearchPartners']['teamcomment'] = true;
-
 $wgGroupPermissions['LFCEvaluators']['teamcomment'] = true;
+
+# For reorganization
+$wgGroupPermissions['TorqueAdmin']['generated'] = true;
+$wgGroupPermissions['TorqueAdmin']['edittorqueconfig'] = true;
+$wgGroupPermissions['TorqueAdmin']['torquedataconnect-admin'] = true;
+$wgGroupPermissions['TorqueAdmin']['teamcomment'] = true;
+$wgGroupPermissions['TorqueAdmin']['teamcommentseeusernames'] = true;
+$wgGroupPermissions['TorqueAdmin']['picksome'] = true;
+$wgGroupPermissions['TorqueAdmin']['picksome-admin'] = true;
+$wgGroupPermissions['TorqueAdmin']['view-special-log'] = true;
+$wgGroupPermissions['TorqueAdmin']['torquedataconnect-edit'] = true;
+$wgGroupPermissions['TorqueAdmin']['emulategroup'] = true;
+
+$wgGroupPermissions['LfcStaff']['edittorqueconfig'] = true;
+$wgGroupPermissions['LfcStaff']['teamcomment'] = true;
+$wgGroupPermissions['LfcStaff']['teamcommentseeusernames'] = true;
+$wgGroupPermissions['LfcStaff']['picksome'] = true;
+$wgGroupPermissions['LfcStaff']['view-special-log'] = true;
+$wgGroupPermissions['LfcStaff']['torquedataconnect-edit'] = true;
+$wgGroupPermissions['LfcStaff']['emulategroup'] = true;
+
+$wgGroupPermissions['TorqueDecisionMaker']['teamcomment'] = true;
+$wgGroupPermissions['TorqueDecisionMaker']['teamcommentseeusernames'] = true;
+$wgGroupPermissions['TorqueDecisionMaker']['picksome'] = true;
+$wgGroupPermissions['TorqueDecisionMaker']['picksome-write'] = true;
+
+$wgGroupPermissions['TorquePartner']['teamcomment'] = true;
+$wgGroupPermissions['TorquePartner']['picksome'] = true;
+
 
 # Disable Special:Log for groups that don't have view-special-log above
 $wgHooks['SpecialPage_initList'][] = function ( &$list ) {
@@ -80,19 +117,30 @@ $wgHooks['SpecialPage_initList'][] = function ( &$list ) {
 # 
 # These are the MediaWiki Groups:
 $wgSimpleSAMLphp_SyncAllGroups_LocallyManaged = [
-  "sysop",
-  "bureaucrat",
-  "interface-admin",
+  # Deprecated groups
   "DecisionMakers",
   "LFCConsultingPartners",
   "LFCResearchPartners",
   "LFCEvaluators",
   "PseudoDecisionMakers",
+
+  # After the reorganization
+  "sysop",
+  "bureaucrat",
+  "interface-admin",
+  "TorqueAdmin",
+  "LfcStaff",
+  "TorqueDecisionMaker",
+  "TorquePartner",
+  "TorqueDiligenceFinancial",
+  "TorqueDiligenceNonFinancial",
+  "TorqueBasic"
 ];
 #
 # And this is the mapping (MediaWiki on left, incoming SSO on right):
 #
 $wgSimpleSAMLphp_GroupMap = [
+  # Deprecated settings
   'sysop' => ['groups' => ['LFC Torque Admin', 'LFC Staff', 'LFC Admins']],
   'interface-admin' => ['groups' => ['LFC Torque Admin', 'LFC Staff', 'LFC Admins']],
   'bureaucrat' => ['groups' => ['LFC Torque Admin', 'LFC Staff', 'LFC Admins']],
@@ -100,7 +148,20 @@ $wgSimpleSAMLphp_GroupMap = [
   'PseudoDecisionMakers' => ['groups' => ['LFC Pseudo Decision Makers', 'Pseudo Board Members']],
   'LFCEvaluators' => ['groups' => ['LFC Evaluators']],
   'LFCConsultingPartners' => ['groups' => ['LFC Consultants']],
-  'LFCResearchPartners' => ['groups' => ['LFC Research Partners']]
+  'LFCResearchPartners' => ['groups' => ['LFC Research Partners']],
+  'LFCTorqueAdmin' => ['groups' => ['LFC Torque Admin', 'LFC Staff', 'LFC Admins']],
+
+  # After the reorganization
+  'sysop' => ['groups' => ['Torque Admin']],
+  'interface-admin' => ['groups' => ['Torque Admin']],
+  'bureaucrat' => ['groups' => ['Torque Admin']],
+  'TorqueAdmin' => ['groups' => ['Torque Admin']],
+  'LfcStaff' => ['groups' => ['LfC Staff']],
+  'TorqueDecisionMaker' => ['groups' => ['Torque Decision Maker']],
+  'TorquePartner' => ['groups' => ['Torque Partner']],
+  'TorqueDiligenceFinancial' => ['groups' => ['Torque Diligence - Financial']],
+  'TorqueDiligenceNonFinancial' => ['groups' => ['Torque Diligence - Non-Financial']],
+  'TorqueBasic' => ['groups' => ['Torque Basic']]
 ];
 
 ?>
