6b405aec1ded8043bbfaa20728828fe2241242f7
Frank Duncan <frank@kank.net>
2022-01-26 09:05:41 -0600

    New Ideas user group for New Ideas wiki

---
+++ competitions/NewIdeas/ansible/roles/NewIdeas/files/newideas.php
@@ -0,0 +1,15 @@
+<?php
+$wgGroupPermissions["NewIdeas"]['read'] = true;
+$wgGroupPermissions['NewIdeas']['edittorqueconfig'] = true;
+$wgGroupPermissions['NewIdeas']['teamcomment'] = true;
+$wgGroupPermissions['NewIdeas']['teamcommentseeusernames'] = true;
+$wgGroupPermissions['NewIdeas']['picksome'] = true;
+$wgGroupPermissions['NewIdeas']['view-special-log'] = true;
+$wgGroupPermissions['NewIdeas']['torquedataconnect-edit'] = true;
+$wgGroupPermissions['NewIdeas']['emulategroup'] = true;
+$wgGroupPermissions['NewIdeas']['lfcanalysis'] = true;
+
+array_push($wgSimpleSAMLphp_SyncAllGroups_LocallyManaged, "NewIdeas");
+
+$wgSimpleSAMLphp_GroupMap["NewIdeas"] = ['groups' => ['New Ideas']];
+?>
